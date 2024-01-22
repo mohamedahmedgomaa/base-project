d8a166c90ecf172d3f38b507f22e1cc0f8d39cf0
mido1 <mido.15897@gmail.com>
2024-01-22 10:15:37 +0300

    Update composer and add .gitignore

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,20 +1,20 @@
 <?php return array(
     'root' => array(
         'name' => 'gomaa/test',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'd3d77ccca9bb2c69117b4c6406ebd895d8991e46',
-        'type' => 'project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8388f378a33be80b9d367b32ac81d1474b561060',
+        'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
         'gomaa/test' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'd3d77ccca9bb2c69117b4c6406ebd895d8991e46',
-            'type' => 'project',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8388f378a33be80b9d367b32ac81d1474b561060',
+            'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
