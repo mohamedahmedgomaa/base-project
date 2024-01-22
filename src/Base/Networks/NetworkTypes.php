341bf078fe4441f3070ce0900dafe1ad86557fe3
mido1 <mido.15897@gmail.com>
2024-01-22 11:02:44 +0300

    Add Base Project

---
+++ src/Base/Networks/NetworkTypes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Gomaa\Test\Base\Networks;
+
+interface NetworkTypes
+{
+    const OPS = "ops";
+
+    const LOCAL = "_local";
+    const STAGE = "_staging";
+    const PROD = "_production";
+}
