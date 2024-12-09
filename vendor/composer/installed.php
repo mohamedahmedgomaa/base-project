12e27b16b7e3f96d67c0cafd28f4cc571f41b722
Mohamed Gomaa <mohammed.gomaa@munjz.com>
2024-12-09 14:54:39 +0300

    Update composer.json add version 2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'gomaa/test',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => 'ff272e4718ac43d1c49be8eebc294e496fc8aef3',
+        'pretty_version' => '1.0.0',
+        'version' => '1.0.0.0',
+        'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'gomaa/test' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => 'ff272e4718ac43d1c49be8eebc294e496fc8aef3',
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
