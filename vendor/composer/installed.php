84944832d5b91319eebc0dd45d2932901036584a
Mohamed Gomaa <mido.15897@gmail.com>
2025-04-12 17:46:40 +0200

    Base Update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'gomaa/base',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c676b68dd94027f97763770ead816a07925ca36c',
+        'reference' => 'af41a7160bf9db269d2572409c3b679bc595fc5a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'gomaa/base' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c676b68dd94027f97763770ead816a07925ca36c',
+            'reference' => 'af41a7160bf9db269d2572409c3b679bc595fc5a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
